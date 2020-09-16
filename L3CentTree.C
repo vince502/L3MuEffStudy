9d285a4408d164ca0f8b04a8f13597d8e00caefb
Soohwan Lee <soohwan.lee@cern.ch>
2020-09-16 21:01:46 +0900

    code update

--- L3CentTree.C
+++ L3CentTree.C
@@ -11,23 +11,25 @@
 
 void L3CentTree(){
 
-  TFile* l3t = new TFile("Large_Files/8v_L3.root", "update");
-//  TFile* resl3 = new TFile("L3_result.root", "recreate");
+  TFile* l3t = new TFile("Large_Files/1.1v_MBL3.root", "read");
+  TFile* resl3 = new TFile("L3_result.root", "recreate");
 
   std::string reco = "/Users/soohwanlee/RunPreparation/store/Forest_HIMinimumBias2_run327237_merged.root";
 
   RecoReader recoInfo(reco, false);
   TTree* t1 =(TTree*) l3t->Get("l3pAnalyzer/L3Track");
-  
+  TTree *newTree = t1->CloneTree(0);
+ 
   TTreeReader r1 = TTreeReader("l3pAnalyzer/L3Track", l3t);
   TTreeReaderValue<int> Run = {r1, "Run"};
   TTreeReaderValue<int> Event = {r1, "Event"};
-  int centI;
-  t1->Branch("hiBin", &centI, "hiBin/I");
+  int centI=300;
+
+  newTree->Branch("hiBin", &centI, "hiBin/I");
 
   const auto nEntries = recoInfo.getEntries();
   long long count = 0 ;
-  for (int iEntry=0; iEntry <nEntries; ++iEntry){
+  for (int iEntry=0; iEntry <1000; ++iEntry){
     if ((iEntry%100000)==0){std::cout << "Processing event " << iEntry << " / " << nEntries << std::endl;}
     recoInfo.setEntry(iEntry, false, true);
     std::pair<Long64_t, Long64_t> evtInfo = recoInfo.getEventNumber();
@@ -35,25 +37,29 @@ void L3CentTree(){
     count++;
 
         while(r1.Next()){
-	
+
 	  if( evtInfo.first == *Run && evtInfo.second == *Event ){
 //	    std::cout<< "Found hibin"<<", in Event(L3/reco): " << *Event<<" / " << evtInfo.second  << std::endl;
             const auto bObj = recoInfo.getCentrality();
             centI = (int) bObj;
-            t1->Fill();
             break;
           }
+	  centI= 300;
 
         }
 
+
+      newTree->Fill();
+	  std::cout << "test1" << std::endl;
       r1.Restart();
-      centI=0;
 
     }
     std::cout <<"Number of event: "<< count << std::endl;
     r1.Delete();
-    t1->SetTitle("updated_track");
-    l3t->Write();
+    newTree->SetName("updated_track");
     l3t->Close();
+    resl3->cd();
+    newTree->Write();
+    resl3->Close();
 
 }
