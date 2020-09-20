b21738477b93a1f97e27a88478de7873aa6d870d
Soohwan Lee <soohwan.lee@cern.ch>
2020-09-20 15:33:46 +0900

    hist drawer

--- Drawhist.cc
+++ Drawhist.cc
@@ -3,7 +3,7 @@
 #include "TROOT.h"
 #include "TCanvas.h"
 #include "TBranch.h"
-#include "TClass.h"
+//#include "TClass.h"
 #include "TFile.h"
 #include "TTree.h"
 #include <iostream>
@@ -14,44 +14,38 @@
 #include "TH1D.h"
 #include "TObject.h"
 #include "TObjArray.h"
+#include <memory>
 
 void Drawhist(){
   const int Max_mu_size = 10000;
   std::string fname = "MBL3.root";
   TFile* f = new TFile(fname.c_str());
+  TTreeReader r1("L3Track");
   TTree* t1 = TTreeReader("L3Track").GetTree();
+//  TTree* t2 = t1->CloneTree(0);
   TObjArray* blist = t1->GetListOfBranches();
-  t1->SetBranchStatus("*",0);
   TCanvas* c1 = new TCanvas("c1", "EDProducer Pt Collection", 1000, 800);
-  //auto legend = new TLegend(160,10, 180,1000);
+  auto legend = new TLegend(160,10, 180,1000);
   long long nEntries = t1->GetEntries();
-  int* Event;
-  int* Run;
+  Int_t Event;
+  Int_t Run;
+  TClonesArray* TC = new TClonesArray("TLorentzVector", Max_mu_size);
   t1->SetBranchAddress("Event", &Event);
   t1->SetBranchAddress("Run", &Run);
+
+
   std::cout << "Set Br address: Evt, Run load Entries" << std::endl;
-  for(int brent=0; brent<13; brent++){
-    
+  for(Int_t brent=0; brent<13; brent++){    
     TBranch* br = (TBranch*) blist->At(brent+2);
-    std::string pname = t1->GetListOfBranches()->At(brent+2)->GetName(); 
-    TObject* obj;;
-    std::cout << pname << std::endl;
-    t1->SetBranchStatus(pname.c_str(), 1);
-    t1->SetBranchAddress( pname.c_str(), &obj, &br);
-//    br->SetAddress(&obj);
-    std::cout << "Set Br address: Evt, Run load Entries" << std::endl;
+     std::string pname = br->GetName(); 
+    t1->SetBranchAddress(pname.c_str(), &TC);
     TH1D* hist = new TH1D("hist", pname.c_str(), 20,0,80); 
-//    legend->AddEntry(hist, pname.c_str(), "l");
+    legend->AddEntry(hist, pname.c_str(), "l");
     std::cout << "Reading Branch: " << pname << " with entries " << nEntries << std::endl; 
 
     for(long long j = 0 ; j <nEntries; j++){
-      std::cout << "Tree: " << t1->GetName() << std::endl;
       t1->GetEntry(j);
-      std::cout << "really;;;" << std::endl;;
-      TObjArray* TC = (TObjArray*) obj;
-      long long cEntries = TC->GetSize();
-
- //     std::cout << "Load Entries" << std::endl;
+      int cEntries = TC->GetSize();
       if(cEntries>0){for(int k =0; k < cEntries; k++){
 	/*if(k==0){std::cout << "Start filling" << std::endl;
         std::cout << pname << ": Entry Size -> " << cEntries << "at Event"   << std::endl;
@@ -67,11 +61,11 @@ void Drawhist(){
     hist->SetMarkerStyle(brent+1);
     hist->Draw("CP SAME");
     hist->Clear();
-//    legend->Draw();
+    legend->Draw();
     }
 
   c1->SetLogy();
   c1->Print("Result.pdf"); 
   c1->Close();
- 
+  f->Close(); 
 }
