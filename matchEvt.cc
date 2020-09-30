49fc59ee8c77518f132c230c880c3754f05bfb43
Soohwan Lee <soohwan.lee@cern.ch>
2020-09-30 15:23:45 +0900

    up

--- matchEvt.cc
+++ matchEvt.cc
@@ -23,7 +23,7 @@ void matchEvt(){
   t1->SetBranchAddress("Event", &oEvt);
   
   std::ofstream out;
-  out.open("Eventpair.txt");
+  out.open("realEventpair.txt");
   std::vector<std::pair<int , std::pair<Int_t, long long> > > evlist;
 
   auto maxevt = std::max(oEntries, nEntries);
@@ -39,12 +39,32 @@ void matchEvt(){
       auto p1 = std::make_pair(oEvt, recoEvt); 
       auto p2 = std::make_pair(iEntry, p1);
       evlist.push_back(p2);
-    //  out << iEntry<< " " << oEvt<< " " << recoEvt<< std::endl;
+//      out << iEntry<< " " << oEvt<< " " << recoEvt<< std::endl;
     }
     else if(oEntries > nEntries){std::cout << "Not all online event Matches RECO!!" << std::endl;}
   }
   std::cout << std::endl;
-
+/*
+  std::vector<std::pair<int , std::pair<Int_t, long long> > > sevlist;
+  TStopwatch* clock2 = new TStopwatch();
+  clock2->Start();
+  for( int iEntry = 0; iEntry < maxevt; iEntry++){
+    if((iEntry%100000)==0) {std::cout << "Running Entry: " << iEntry << "\r"; std::cout.flush();}
+    long long ooEvt = evlist[iEntry].second.first;
+    long long rrEvt = evlist[iEntry].second.second;
+    if(oEntries < nEntries +1){
+      if(iEntry > oEntries){oEvt = -1;}
+      auto p3 = std::make_pair(ooEvt, rrEvt);
+      auto p4 = std::make_pair(iEntry, p3);
+      sevlist.push_back(p4);
+//      out << iEntry<< " " << oEvt<< " " << recoEvt<< std::endl;
+    }
+    else if(oEntries > nEntries){std::cout << "Not all online event Matches RECO!!" << std::endl;}
+  }
+  clock2->Stop();
+  Double_t el = clock2->CpuTime();
+  std::cout << el << std::endl;
+*/
   //initialize Index vector
   std::vector<int> index;
   for(int j =0; j<maxevt; j++){
@@ -61,13 +81,13 @@ void matchEvt(){
     auto ooo = p3.second.first;
     for(auto k : index){
       auto px = evlist[k];
-      if((k%10000)==0 && (j%100)==0){std::cout << "on/reco: " << p3.first << "/" << px.first << ". Average rate: " << trate << "(find/s) and going down..."<< "\r"; std::cout.flush();}
+      if((k%10000)==0 && (j%100)==0){std::cout << "on/reco: " << p3.first << "/" << px.first << ". Average rate: " << 1/trate << "(find/s) and going down..."<< "\r"; std::cout.flush();}
       auto rrr = px.second.second;
       if(rrr == ooo){
 	auto pr = std::make_pair(p3.first,px.first);
 	revlist.push_back(pr);
 	index.erase(index.begin()+k);
-	out << p3.first << " "<< px.first << std::endl;
+//	out << p3.first << " "<< px.first << std::endl;
 	break;
       }
     }
@@ -77,5 +97,8 @@ void matchEvt(){
     clock->Reset();
   }
   std::cout << std::endl;
+  for(int j =0; j < revlist.size(); j++){
+    out << revlist[j].first<<" "<<revlist[j].second<< std::endl;
+  }
 }
 
